14f9b2606f388537e1842291e30514129a87d39c
Aji Sahputra <aji.saputro@studens.amikom.ac.id>
2020-09-11 03:09:30 +0700

    Initial Commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Aji Sahputra78 on 11/09/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
